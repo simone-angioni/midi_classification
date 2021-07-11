df891e95de771e5f5e7906395f28c675050cd64f
Simone Angioni <simoangi94@libero.it>
2021-07-11 18:38:01 +0200

    Bug Fix

--- transformer_classifier_colab.py
+++ transformer_classifier_colab.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 from tensorflow.keras import backend as K
 
-physical_devices = tf.config.experimental.list_physical_devices('CPU')
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
 #assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
 #config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
