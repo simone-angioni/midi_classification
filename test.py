3de06b0a457e59640d6c2c84a28df581b0d50681
Simone Angioni <simoangi94@libero.it>
2021-08-20 11:03:13 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 import main_classifier as classifier
 
-classifier.classify("standard", 50, 80, "shuffle_standard_time_frame_50_feature_size_80", True)
+classifier.classify("walk-forward", 50, 80, "shuffle_walk-forward_time_frame_50_feature_size_80", True)
+classifier.classify("walk-forward", 50, 50, "shuffle_walk-forward_time_frame_50_feature_size_50", True)
+classifier.classify("walk-forward", 50, 30, "shuffle_walk-forward_time_frame_50_feature_size_30", True)
